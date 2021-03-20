f2a58ab8529d644155d5e7599114986513ed7016
ergunkargun <ergunkargun@gmail.com>
2021-03-20 23:13:20 +0300

    Fix yapıldı, schedule otomatikleştirildi

--- src/main/java/com/example/solutioncube/job/DailyJobScheduler.java
+++ src/main/java/com/example/solutioncube/job/DailyJobScheduler.java
@@ -33,7 +33,7 @@ public class DailyJobScheduler {
 	@PostConstruct
 	public void scheduleDailyJob() {
 
-		TimeZone timeZone = Calendar.getInstance().getTimeZone();
+		TimeZone timeZone = Calendar.getInstance().getTimeZone(); 
 		LocalDateTime localDateTime = LocalDateTime.of(LocalDateTime.now().toLocalDate(), LocalTime.MIDNIGHT);
 		ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, timeZone.toZoneId());
 
