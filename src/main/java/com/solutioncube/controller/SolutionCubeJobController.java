362250aca46ed3d0ab0cfab34b4473e87901c99e
Samet Öztürk <ozturkksamett@gmail.com>
2021-11-05 01:16:01 +0300

    ..

--- src/main/java/com/solutioncube/controller/SolutionCubeJobController.java
+++ src/main/java/com/solutioncube/controller/SolutionCubeJobController.java
@@ -107,9 +107,9 @@ public class SolutionCubeJobController {
 
 		logger.info("vanucciRunBulkData");
 		CacheManager.clear();
-		parameterGenerator.generateTaskParameter(VANUCCI_CONFIG_INDEX).getMongoTemplate().getDb().drop();
-		serviceRunner.runService(vanucciService, ExecutionType.STATIC_COLLECTIONS, true);
-		serviceRunner.runService(vanucciService, ExecutionType.BULK_DATA_WITH_BOTH_SINCE_AND_TILL_PARAM, false);
+//		parameterGenerator.generateTaskParameter(VANUCCI_CONFIG_INDEX).getMongoTemplate().getDb().drop();
+//		serviceRunner.runService(vanucciService, ExecutionType.STATIC_COLLECTIONS, true);
+//		serviceRunner.runService(vanucciService, ExecutionType.BULK_DATA_WITH_BOTH_SINCE_AND_TILL_PARAM, false);
 		serviceRunner.runService(vanucciService, ExecutionType.BULK_DATA_ONLY_WITH_SINCE_PARAM, false);
 		return "Vanucci finished running for bulk data successfully";
 	}
