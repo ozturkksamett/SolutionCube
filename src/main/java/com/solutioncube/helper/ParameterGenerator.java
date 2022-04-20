6b651703384d5761c38152d51c2d21e33b2a73a3
Samet Öztürk <ozturkksamett@gmail.com>
2022-04-20 03:52:04 +0300

    ..

--- src/main/java/com/solutioncube/helper/ParameterGenerator.java
+++ src/main/java/com/solutioncube/helper/ParameterGenerator.java
@@ -18,7 +18,7 @@ public class ParameterGenerator {
 	private static final Logger logger = LoggerFactory.getLogger(ParameterGenerator.class);
 	
 	private static final int INTERVAL_DAY = 30;
-	private static LocalDate initialDate = LocalDate.of(2022, 03, 22);
+	private static LocalDate initialDate = LocalDate.of(2022, 04, 07);
 	public static boolean isBulkData = false;
 	
 	@Autowired
