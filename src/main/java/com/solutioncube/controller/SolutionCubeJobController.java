10c55ef9e7ea5a4e18aa136d0cabfcd18a7e33a1
Samet Öztürk <ozturkksamett@gmail.com>
2022-05-11 01:25:30 +0300

    ...

--- src/main/java/com/solutioncube/controller/SolutionCubeJobController.java
+++ src/main/java/com/solutioncube/controller/SolutionCubeJobController.java
@@ -107,8 +107,7 @@ public class SolutionCubeJobController {
 	public String vanucciRunBulkData() {
 
 		logger.info("vanucciRunBulkData");
-		CacheManager.clear();  
-		
+		CacheManager.clear(); 
 		//serviceRunner.runService(vanucciService, ExecutionType.STATIC_COLLECTIONS, true);
 		serviceRunner.runService(vanucciService, ExecutionType.BULK_DATA_WITH_BOTH_SINCE_AND_TILL_PARAM, false);
 		//serviceRunner.runService(vanucciService, ExecutionType.BULK_DATA_ONLY_WITH_SINCE_PARAM, false);
